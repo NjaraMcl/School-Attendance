3c1ae803c4fbc7f4e12945027654cccdb48fb7b3
NjaraMcl <mamiarisoamarcel@gmail.com>
2022-08-28 21:04:48 +0300

    setting.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -133,7 +133,9 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = [
+    BASE_DIR / "media",
+]
 
 AUTH_USER_MODEL = "users.siteUser"
 LOGIN_REDIRECT_URL = "profile"
