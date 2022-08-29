fe2e503cf9ba3870d38c29f6a1297d9249ff18b4
NjaraMcl <mamiarisoamarcel@gmail.com>
2022-08-29 13:14:50 +0300

    change in setting

--- myProject/settings.py
+++ myProject/settings.py
@@ -123,9 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = str(BASE_DIR / "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -133,9 +132,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = [
-    BASE_DIR / "media",
-]
+MEDIA_ROOT = BASE_DIR / "media"
 
 AUTH_USER_MODEL = "users.siteUser"
 LOGIN_REDIRECT_URL = "profile"
